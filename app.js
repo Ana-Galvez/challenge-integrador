221d1c89c1e9d53b436c46fae7e007a44583f03f
Macarena Amarilla <121466934+MacarenaAmarilla@users.noreply.github.com>
2023-11-11 11:37:02 -0300

    carga de carpetas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mainRoutes = require("./src/routes/mainRoutes.js");
 const authRoutes = require("./src/routes/authRoutes.js");
 const adminRoutes = require('./src/routes/adminRoutes.js');
+const shopRoutes = require('./src/routes/shopRoutes.js');
 const app = express();
 
 app.use(express.static("public"));
@@ -9,6 +10,7 @@ app.use(express.static("public"));
 app.use("/", mainRoutes);
 app.use("/auth",authRoutes);
 app.use("/admin",adminRoutes);
+app.use("/shop",shopRoutes);
 
 app.listen(4000, () => {
   console.log("Servidor corriendo en http://localhost:4000");
