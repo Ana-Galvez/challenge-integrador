ceb3234448f3ce45162428769157843074aa204c
EduRiu <chino_926@hotmail.com>
2023-11-10 21:39:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mainRoutes = require("./src/routes/mainRoutes.js");
 const authRoutes = require("./src/routes/authRoutes.js");
+const adminRoutes = require('../src/routes/adminRoutes.js');
 const app = express();
 
 app.use(express.static("public"));
 
 app.use("/", mainRoutes);
 app.use("/auth",authRoutes);
+app.use("/admin",adminRoutes);
 
 app.listen(4000, () => {
   console.log("Servidor corriendo en http://localhost:4000");
