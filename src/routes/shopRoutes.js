1fa0e2fcd9b7ce03f51090d9afb183d120052bf8
Macarena Amarilla <121466934+MacarenaAmarilla@users.noreply.github.com>
2023-11-11 12:21:22 -0300

    modificacion de verbo get por post

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -6,7 +6,7 @@ router.get("/",shopController.shop_get);
 router.get("/item/:id", shopController.itemId_get);
 router.post("/item/:id/add", shopController.itemIdAdd_post);
 router.get("/cart", shopController.cart_get);
-router.get("/cart", shopController.cart_post);
+router.post("/cart", shopController.cart_post);
 
 
 module.exports = router;
