b3c4aa97d6b4459774703e410b3f0ae8ad26b183
EduRiu <chino_926@hotmail.com>
2023-11-10 21:46:21 -0300

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,9 +3,9 @@ const express =  require('express');
 const router = express.Router();
 
 
-router.GET("/admin",adminController.admin_get);
-router.GET("/admin/create", adminController.create_get);
-router.POST("/admin/create", adminController.create_post);
-router.GET("/admin/edit/:id", adminController.editID_get);
-router.PU("/admin/edit/:id", adminController.editID_put);
-router.DELETE("/admin/delete/:id", adminController.editID_delete);
+router.GET("/",adminController.admin_get);
+router.GET("/create", adminController.create_get);
+router.POST("/create", adminController.create_post);
+router.GET("/edit/:id", adminController.editID_get);
+router.PU("/edit/:id", adminController.editID_put);
+router.DELETE("/delete/:id", adminController.editID_delete);
