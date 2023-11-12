0e28bd75aa62f0cfe6b855bf11920860b68d8db3
EduRiu <chino_926@hotmail.com>
2023-11-12 11:59:54 -0300

    bug fix / create partinels headers

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,10 +1,10 @@
 const adminController = {
-    admin_get: (req,res)=>res.send("Ruta para admin"),
-    create_get: (req,res)=>res.send("Ruta para enviar solictud crear usuario"),
-    create_post: (req,res)=>res.send("Ruta para impactar usuario creado admin"),
-    editID_get: (req,res)=>res.send("Ruta para enviar edit ID"),
-    editID_put: (req,res)=>res.send("Ruta para impactar edit ID"),
-    editID_delete: (req,res)=>res.send("Ruta para borrar"),
+    admin_get: (req,res)=>res.render("admin",{title:"ADMIN | FUNKOSHOP"}),
+    create_get: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
+    create_post: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
+    editID_get: (req,res)=>res.render("edit",{title:"EDITAR| FUNKOSHOP"}),
+    editID_put: (req,res)=>res.render("create",{title:"EDITAR| FUNKOSHOP"}),
+    editID_delete: (req,res)=>res.render("create",{title:"BORRAR | FUNKOSHOP"}),
 }
 
 module.exports = adminController    
