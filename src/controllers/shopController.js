950eb0fc699077ebfda27fbe53a62f565f5faaac
Ana-Galvez <anamariagalvez01@gmail.com>
2023-11-14 15:30:30 -0300

    Comentarios para Maca

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,4 +1,5 @@
 const shopController = {
+   // FIJATE COMO PONER PARA QUE LEA EL JSON, EN MAINCONTROLLER O ADMINCONTROLLER
    shop_get: (req,res)=>res.render("shop",{title:"SHOP | FUNKOSHOP"}),
    itemId_get: (req,res)=>res.render("item",{title:"ITEM | FUNKOSHOP"}),
    itemIdAdd_post: (req,res)=>res.render("item",{title:"ITEM | FUNKOSHOP"}),
