48c7a6edee5000637b7189961902b21e522ce229
EduRiu <chino_926@hotmail.com>
2023-11-13 22:27:04 -0300

    dinamismo en adminController y vista admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+
 const adminController = {
-    admin_get: (req,res)=>res.render("admin",{title:"ADMIN | FUNKOSHOP"}),
+    admin_get: (req,res)=>{
+        const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
+        const product = JSON.parse(fileJson);
+        res.render("admin",{title:"ADMIN | FUNKOSHOP",products:product})
+    },
     create_get: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
     create_post: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
     editID_get: (req,res)=>res.render("edit",{title:"EDITAR| FUNKOSHOP"}),
