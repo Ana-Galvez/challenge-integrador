c7834e715f37dbad1644710e6b02e7c477621425
EduRiu <chino_926@hotmail.com>
2023-11-13 23:06:10 -0300

    add views for error400/&403

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -6,7 +6,17 @@ function error500(req, res) {
   res.status(500).render("error500",{title:"Error al cargar"});
 };
 
+function error400(req, res) {
+  res.status(400).render("error400",{title:"Recurso no encontrado"});
+};
+
+function error403(req, res) {
+  res.status(403).render("error403",{title:"Acceso denegado"});
+};
+
 module.exports = {
   error404,
-  error500
+  error500,
+  error400,
+  error403
 };
