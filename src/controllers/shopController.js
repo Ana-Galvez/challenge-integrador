b8e757175310e4d918ab97470c437a83d3dddb96
Ana-Galvez <anamariagalvez01@gmail.com>
2023-11-14 22:21:08 -0300

    comentarios para Maca

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,6 +1,14 @@
+//importar fs path
+
+
+
 const shopController = {
    // FIJATE COMO PONER PARA QUE LEA EL JSON, EN MAINCONTROLLER O ADMINCONTROLLER
-   shop_get: (req,res)=>res.render("shop",{title:"SHOP | FUNKOSHOP"}),
+   //
+   shop_get: (req,res)=>{
+      const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
+      const product = JSON.parse(fileJson);
+      res.render("shop",{title:"SHOP | FUNKOSHOP",products:product})},
    itemId_get: (req,res)=>res.render("item",{title:"ITEM | FUNKOSHOP"}),
    itemIdAdd_post: (req,res)=>res.render("item",{title:"ITEM | FUNKOSHOP"}),
    cart_get: (req,res)=>res.render("cart",{title:"CARRITO | FUNKOSHOP"}),
