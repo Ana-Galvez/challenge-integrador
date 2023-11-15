41e7b467732aeeb57f3592acdafcdd988facc8cf
Ana-Galvez <anamariagalvez01@gmail.com>
2023-11-15 13:48:35 -0300

    [FEATURE] carrousel con foreach item.ejs,shopController

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -6,7 +6,10 @@ const shopController = {
       const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
       const product = JSON.parse(fileJson);
       res.render("shop",{title:"SHOP | FUNKOSHOP",products:product})},
-   itemId_get: (req,res)=>res.render("item",{title:"ITEM | FUNKOSHOP"}),
+   itemId_get: (req, res) => {
+      const fileJson = fs.readFileSync(path.join(__dirname, "../../products.json"));
+      const product = JSON.parse(fileJson);
+      res.render("item", { title: "ITEM | FUNKOSHOP",products:product })},
    itemIdAdd_post: (req,res)=>res.render("item",{title:"ITEM | FUNKOSHOP"}),
    cart_get: (req,res)=>res.render("cart",{title:"CARRITO | FUNKOSHOP"}),
    cart_post: (req,res)=>res.render("cart",{title:"CARRITO | FUNKOSHOP"}),
