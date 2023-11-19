cf52ecb407b36e414abf6a741ad4ea4d1bdc0b1a
Ana-Galvez <anamariagalvez01@gmail.com>
2023-11-19 16:16:23 -0300

    agrego para el profe error de express validator

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,20 @@
 const authController = require("../controllers/authController.js");
 const express = require("express");
+const { body } = require('express-validator')
+const validateInput = require('../middlewares/validador.js')
 const router = express.Router();
 
+const loginValidation = [
+  body('mail')
+    .isEmail()
+    .withMessage('Correo no válido, verifique'),
+  body('contrasena')
+    .isLength({ min: 8 })
+    .withMessage('Contraseña mínima 8 caracteres con letras y números')
+]
+
 router.get("/login",authController.login_get);
-router.post("/login",authController.login_post);
+router.post("/login",loginValidation,validateInput,authController.login_post);
 router.get("/register",authController.register_get);
 router.post("/register",authController.register_post);
 router.get("/logout",authController.logout);
