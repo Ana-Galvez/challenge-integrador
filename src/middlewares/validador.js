262486c77355743c38e94d60d0ecdedf7bbd951a
EduRiu <chino_926@hotmail.com>
2023-11-21 18:05:47 -0300

    validatorOK para Loggin

--- src/middlewares/validador.js
+++ src/middlewares/validador.js
@@ -4,7 +4,7 @@ const validateInput = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-   res.status(400).send({ errors: errors.array() });
+   return res.status(400).json({ errors: errors.array() });
     // return res.status(400).render("error400",{title:"Recurso no encontrado"});
   }
   next();
