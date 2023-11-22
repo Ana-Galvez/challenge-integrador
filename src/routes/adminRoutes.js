a5137adacf68a3212432b3060f07f441d7c92790
Ana-Galvez <anamariagalvez01@gmail.com>
2023-11-22 17:00:33 -0300

    prueba

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -6,20 +6,21 @@ const validateInput = require('../middlewares/validador')
 const {body} = require('express-validator')
 
 
-const createAndLoginValidation = [
-  body('mail')
-    .isEmail()
-    .withMessage('Correo no válido, verifique'),
-  body('contrasena')
-    .isLength({ min: 8 })
-    .withMessage('Contraseña mínima 8 caracteres con letras y números')
-]
+// const createAndLoginValidation = [
+//   body('mail')
+//     .isEmail()
+//     .withMessage('Correo no válido, verifique'),
+//   body('contrasena')
+//     .isLength({ min: 8 })
+//     .withMessage('Contraseña mínima 8 caracteres con letras y números')
+// createAndLoginValidation,validateInput,
+// ]
 
 router.get("/",adminController.admin_get); // vista admin
-router.get("/create", uploadfile.array('imagenes',2),    adminController.create_get); // vista crear
-router.post("/create", createAndLoginValidation,validateInput,adminController.create_post); // vista creado VER VALIDACIÓN
+router.get("/create",adminController.create_get); // vista crear
+router.post("/create", uploadfile.array('imagenes',2),adminController.create_post); // vista creado VER VALIDACIÓN
 router.get("/edit/:id", adminController.editID_get); // viste editar
-router.put("/edit/:id",adminController.editID_put); // vista editado VER VALIDACIÓN
+router.put("/edit/:id",uploadfile.array('imagenes',2),adminController.editID_put); // vista editado VER VALIDACIÓN
 router.delete("/delete/:id", adminController.editID_delete);
 
 module.exports = router;
