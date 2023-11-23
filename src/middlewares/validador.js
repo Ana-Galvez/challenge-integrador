d965fa2fca48fd049af3907d1b200bad76b0b781
Ana-Galvez <anamariagalvez01@gmail.com>
2023-11-23 16:27:29 -0300

    pruebas con express validator create y edit

--- src/middlewares/validador.js
+++ src/middlewares/validador.js
@@ -4,7 +4,7 @@ const validateInput = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).render("error400",{title:"Verique nuevamente los datos ingresados"});
+    return res.status(400).json({ errors: errors.array() });
   }
   next();
 };
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,21 +5,41 @@ const uploadfile = require('../middlewares/upload')
 const validateInput = require('../middlewares/validador')
 const {body} = require('express-validator')
 
-
-const createAndLoginValidation = [
+const createAndEditValidation = [
   body('categoria')
-    .withMessage('Correo no válido, verifique'),
-  body('contrasena')
-    .isLength({ min: 8 })
-    .withMessage('Contraseña mínima 8 caracteres con letras y números')
-
+    .notEmpty()
+    .withMessage('Debe seleccionar una opción'),
+  body('licencia')
+    .notEmpty()
+    .withMessage('Debe seleccionar una opción'),
+  body('nombre_producto')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('detalle_producto')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('sku')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('precio')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('stock')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('descuento')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('cuotas')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
 ]
 
 router.get("/",adminController.admin_get); // vista admin
 router.get("/create",adminController.create_get); // vista crear
-router.post("/create", uploadfile.array('imagenes',2),createAndLoginValidation,validateInput,adminController.create_post); // vista creado VER VALIDACIÓN
+router.post("/create", uploadfile.array('imagenes',2),createAndEditValidation,validateInput,adminController.create_post); // vista creado VER VALIDACIÓN
 router.get("/edit/:id", adminController.editID_get); // viste editar
-router.put("/edit/:id",uploadfile.array('imagenes',2),createAndLoginValidation,validateInput,adminController.editID_put); // vista editado VER VALIDACIÓN
+router.put("/edit/:id",uploadfile.array('imagenes',2),createAndEditValidation,validateInput,adminController.editID_put); // vista editado VER VALIDACIÓN
 router.delete("/delete/:id", adminController.editID_delete);
 
 module.exports = router;
