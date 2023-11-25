6a9176073c99ef5ee5846eeda7a79ece3d8e6e4b
Macarena Amarilla <121466934+MacarenaAmarilla@users.noreply.github.com>
2023-11-25 00:11:08 -0300

    realizo ajustes en editid_get
    realizo ajustes en editid_get

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -10,7 +10,12 @@ const adminController = {
     },
     create_get: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
     create_post: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
-    editID_get: (req,res)=>res.render("edit",{title:"EDITAR| FUNKOSHOP"}),
+    editID_get: (req,res)=> {
+        const id  = req.params.id;
+        const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
+        const product = JSON.parse(fileJson);
+        res.render("edit",{title:"EDITAR| FUNKOSHOP",products:product,id})
+    },
     editID_put: (req,res)=>res.render("edit",{title:"EDITAR| FUNKOSHOP"}),
     editID_delete: (req,res)=>res.render("delete",{title:"BORRAR | FUNKOSHOP"}),
 }
