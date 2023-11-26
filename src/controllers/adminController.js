a97115ba469586af0455c6698b655885bfbaeb2c
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2023-11-26 00:30:16 -0300

    agregado bbdd edit

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,3 +1,4 @@
+const modelos = require('../models/products.js');
 const fs = require("fs");
 const path = require("path");
 
@@ -10,11 +11,12 @@ const adminController = {
     },
     create_get: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
     create_post: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
-    editID_get: (req,res)=> {
-        const id  = req.params.id;
-        const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
-        const product = JSON.parse(fileJson);
-        res.render("edit",{title:"EDITAR| FUNKOSHOP",products:product,id})
+    editID_get: async (req, res) => {
+        const id = req.params.id;
+        const allProducts = await modelos.getProducts()
+        // const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
+        // const product = JSON.parse(fileJson);
+        res.render("edit", { title: "EDITAR| FUNKOSHOP",products:allProducts,id })
     },
     editID_put: (req,res)=>res.render("edit",{title:"EDITAR| FUNKOSHOP"}),
     editID_delete: (req,res)=>res.render("delete",{title:"BORRAR | FUNKOSHOP"}),
