407ff89ee8585dd899c6481de998f74ce9caee97
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2023-11-27 15:46:51 -0300

    agregado bbdd admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -4,26 +4,27 @@ const path = require("path");
 
 
 const adminController = {
-    admin_get: (req,res)=>{
-        const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
-        const product = JSON.parse(fileJson);
-        res.render("admin",{title:"ADMIN | FUNKOSHOP",products:product})
+    admin_get: async (req,res)=>{
+        const allProducts = await modelos.getProducts()
+        // const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
+        // const product = JSON.parse(fileJson);
+        res.render("admin",{title:"ADMIN | FUNKOSHOP",products:allProducts})
     },
-    create_get: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
-    create_post: (req,res)=>res.render("create",{title:"CREAR| FUNKOSHOP"}),
+    create_get: (req,res)=>res.render("create",{title:"CREAR PRODUCTO | FUNKOSHOP"}),
+    create_post: (req,res)=>res.render("create",{title:"CREAR PRODUCTO | FUNKOSHOP"}),
     editID_get: async (req, res) => {
         const id = req.params.id;
         const allProducts = await modelos.getProducts()
         // const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
         // const product = JSON.parse(fileJson);
-        res.render("edit", { title: "EDITAR| FUNKOSHOP",products:allProducts,id })
+        res.render("edit", { title: "EDITAR PRODUCTO | FUNKOSHOP",products:allProducts,id })
     },
     editID_put: (req,res)=>{
         const id = req.params.id;
         const fileJson = fs.readFileSync(path.join(__dirname,"../../products.json"));
         const product = JSON.parse(fileJson);
-        res.render("edit",{title:"EDITAR | FUNKOSHOP",products:product,id})},
-    editID_delete: (req,res)=>res.render("delete",{title:"BORRAR | FUNKOSHOP"}),
+        res.render("edit",{title:"EDITAR PRODUCTO| FUNKOSHOP",products:product,id})},
+    editID_delete: (req,res)=>res.render("delete",{title:"BORRAR PRODUCTO | FUNKOSHOP"}),
 }
 
 module.exports = adminController    
