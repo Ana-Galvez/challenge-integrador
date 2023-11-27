790e01ef9eb76a589d7f224296ccb8743df09786
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2023-11-27 15:49:53 -0300

    arreglo sql getProducts

--- src/models/products.js
+++ src/models/products.js
@@ -2,7 +2,7 @@ const {connection} = require('../config/conn.js')
 
 const getProducts = async () => {
   try {
-    const [rows] = await connection.query('SELECT * FROM product JOIN licence ON product.licence_id = licence.licence_id JOIN category ON product.category_id = category.category_id;')
+    const [rows] = await connection.query('SELECT * FROM product JOIN licence ON product.licence_id = licence.licence_id JOIN category ON product.category_id = category.category_id ORDER BY product_id;')
     return rows
   } catch (error) {
     throw error
