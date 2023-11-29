cf1095e1605f786f00c3379bb6ac2489fe442de7
Macarena Amarilla <121466934+MacarenaAmarilla@users.noreply.github.com>
2023-11-29 00:45:34 -0300

    VALIDADOR DEL CREATE Y EDITE

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,14 +5,49 @@ const uploadfile = require('../middlewares/upload')
 const validateInput = require('../middlewares/validador')
 const {body} = require('express-validator')
 
+const adminController = require("../controllers/adminController")
+const express =  require('express');
+const router = express.Router();
+const uploadfile = require('../middlewares/upload') 
+const validateInput = require('../middlewares/validador')
+const {body} = require('express-validator')
+
+const createAndEditValidation = [
+  body('categoria')
+    .notEmpty()
+    .withMessage('Debe seleccionar una opción'),
+  body('licencia')
+    .notEmpty()
+    .withMessage('Debe seleccionar una opción'),
+  body('nombre_producto')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('detalle_producto')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('sku')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('precio')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('stock')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('descuento')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+  body('cuotas')
+    .notEmpty()
+    .withMessage('El campo no debe estar vacío'),
+]
 
 router.get("/",adminController.admin_get); // vista admin
 router.get("/create",adminController.create_get); // vista crear
-router.post("/create", uploadfile.array('imagenes',2),adminController.create_post); // vista creado
-router.get("/edit/:id", adminController.editID_get); // viste editar
-router.put("/edit/:id", uploadfile.array('imagenes',2),adminController.editID_put); // vista editado
+router.post("/create", uploadfile.array('imagenes',2),adminController.createAndEditValidation,validateInput,adminController.create_post); // vista creado
+router.get("/edit/:id", adminController.editID_get); // vistA editar
+router.put("/edit/:id",uploadfile.array('imagenes',2),createAndEditValidation,validateInput,adminController.editID_put); // vista editado 
 router.delete("/delete/:id", adminController.editID_delete);
 
-
-
 module.exports = router;
+
