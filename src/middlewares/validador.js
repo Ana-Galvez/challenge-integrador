7b8ffc3e7c576007ee35aa22b7e08deb41c742cb
EduRiu <chino_926@hotmail.com>
2023-11-28 22:27:50 -0300

    validacionTerminada

--- src/middlewares/validador.js
+++ src/middlewares/validador.js
@@ -4,8 +4,9 @@ const validateInput = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-   return res.status(400).json({ errors: errors.array() });
-    // return res.status(400).render("error400",{title:"Verifique los datos"});
+   //return res.status(400).json({ errors: errors.array() });
+    return res.status(400).render("error400",{title:"Verifique los datos"}, res.redirect("/home"))
+   
   }
   next();
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -15,29 +15,45 @@ const loginValidation = [
 
 const registerValidation = [
   body("nombre")
-  .notEmpty()
-    .isString()
+    .notEmpty()
+    .withMessage("no puede estar vacio")
     .isLength({ min: 3, max: 20 })
-    .withMessage("Minimo 3 caracteres"),
+    .withMessage("Minimo 3 caracteres")
   ,
   body("apellido")
   .notEmpty()
-    .isString()
-    .isLength({ min: 2, max: 20 })
-    .withMessage("Minimo 3 caracteres"),
+  .withMessage("no puede estar vacio")
+        .isLength({ min: 2, max: 20 })
+    .withMessage("Minimo 3 caracteres")
   ,
   body("mail")
   .notEmpty()
+  .withMessage("no puede estar vacio")
   .isEmail()
-  .withMessage("Correo no válido, verifique"),
-  body("contrasena").isLength({ min: 6 }).isAlphanumeric()
+  .withMessage("Correo no válido, verifique")
+  ,
+  body("contrasena")
+  .notEmpty()
+  .withMessage("no puede estar vacio")
+  .isLength({ min: 6 })
+  .withMessage("minimo 6 caracteres")
+  .isAlphanumeric()
+  .withMessage("atencion deber ser alfanumerica")
+  ,
+  body("rep_contrasena")
+  .custom((value, {req}) =>{
+    if (value !== req.body.contrasena) {
+        throw new Error('no son iguales la contraseñas')
+    }
+    return true;
+
+  })
 ];
 
 router.get("/login", authController.login_get);
 router.post("/login", loginValidation, validateInput,  authController.login_post);
 router.get("/register", authController.register_get);
-router.post("/register",  authController.register_post
-);
+router.post("/register", registerValidation, validateInput,  authController.register_post);
 router.get("/logout", authController.logout);
 
 module.exports = router;
