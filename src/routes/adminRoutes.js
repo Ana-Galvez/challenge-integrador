3c09cfcfd63c8f786b5c1948eb9b9eae53aff7ff
Ana-Galvez <anamariagalvez01@gmail.com>
2023-12-03 15:21:58 -0300

    agregado validator en edit

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -45,7 +45,7 @@ router.get("/",adminController.admin_get); // vista admin
 router.get("/create",adminController.create_get); // vista crear
 router.post("/create", uploadfile.array('imagenes',2),createAndEditValidation,validateInput,adminController.create_post); // vista creado
 router.get("/edit/:id", adminController.editID_get); // vistA editar
-router.put("/edit/:id",uploadfile.array('imagenes',2),adminController.editID_put); // vista editado 
+router.put("/edit/:id",uploadfile.array('imagenes',2),createAndEditValidation,validateInput,adminController.editID_put); // vista editado 
 router.get("/delete/:id", adminController.deleteID_get);
 router.delete("/delete/:id", adminController.deleteID_delete);
 
