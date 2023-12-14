f026a8f663b7c4b1603ed0067728de825398e6ca
ANA MARIA GALVEZ <130255411+Ana-Galvez@users.noreply.github.com>
2023-12-14 15:39:17 -0300

    Update conn.js

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
-//require('dotenv').config({path:'../../.env'});
+require('dotenv').config();
 
 const conn = mysql.createPool({
   // host: "localhost",
   // user: "root",
   // database: "funko_test",
   // password: "root",
-  /*
-  host:process.env.HOST,
-  user:process.env.USER ,
-  databas 
-  password:process.env.PASS,
-  */
+
+  host:process.env.DBHOST,
+  user:process.env.DBUSER ,
+  port:process.env.DBPORT,
+  password:process.env.DBPASS,
+  database:process.env.DB,
+
 
   // host: "sql10.freesqldatabase.com",
   // user: "sql10627826",
