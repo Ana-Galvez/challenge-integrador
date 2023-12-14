bb14b21c3cbb7e7923f92b7f3e24cac99798f664
ANA MARIA GALVEZ <130255411+Ana-Galvez@users.noreply.github.com>
2023-12-13 22:33:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(methodOverride('_method'))
 
+
 app.use("/", mainRoutes);
 app.use("/auth",authRoutes);
 app.use("/admin",adminRoutes);
