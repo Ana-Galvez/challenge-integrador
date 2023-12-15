7c7bc16a58a2151dd2f497d5d192af1efe911763
ANA MARIA GALVEZ <130255411+Ana-Galvez@users.noreply.github.com>
2023-12-15 19:01:18 -0300

    puntos y comas

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -108,7 +108,7 @@ const adminController = {
     }else{
     res.redirect("/admin");
     }
-  }
+  };
 
   deleteID_get: async (req, res) => {
     const id = req.params.id;
@@ -123,7 +123,7 @@ const adminController = {
       allLicences,
       id,
     });
-  },
+  };
 
   deleteID_delete: async (req, res) => {
     const id = req.params.id;
